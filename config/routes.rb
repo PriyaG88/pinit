dba5385fe5b68c33ca9c37cc64bfb4bb50a6dd53
PriyaG88 <priya.g71488@gmail.com>
2018-04-18 23:15:58 -0700

    Update routes to include session routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: %i[create show]
+    resources :sessions, only: %i[create destroy]
   end
-  
+
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
