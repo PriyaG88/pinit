ac259bd6713cae651920a05c8db21b622d4ce126
PriyaG88 <priya.g71488@gmail.com>
2018-04-18 22:40:11 -0700

    Update users resource so that only create and show are used

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %i[create show]
+  end
+  
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
