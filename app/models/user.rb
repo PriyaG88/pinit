4dfa51b6e46471f6fd7915ebd92e83c9588a4fd4
PriyaG88 <priya.g71488@gmail.com>
2018-04-18 23:16:30 -0700

    Add reset session token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,12 @@ class User < ApplicationRecord
     return user if user && user.is_password?(password)
   end
 
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
