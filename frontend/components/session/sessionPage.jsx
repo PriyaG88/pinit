0163b51ef713434131a8476d1804917dc7055c27
PriyaG88 <priya.g71488@gmail.com>
2018-04-20 15:04:01 -0700

    Remove unnecessary return statement

--- frontend/components/session/sessionPage.jsx
+++ frontend/components/session/sessionPage.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import SessionForm from './SessionForm';
+import SessionFormContainer from './sessionFormContainer';
 
-const SessionPage = () => {
-  return (
-    <div className='session-page-container'>
-      <div className='session-background' />
-      <SessionForm />
-      <div className='session-info-container'>
-        <p>Pinterest helps you find ideas to try.</p>
-        <button type='button' className='info-btn'>How it works</button>
-      </div>
+const SessionPage = () => (
+  <div className='session-page-container'>
+    <div className='session-background' />
+    <SessionFormContainer />
+    <div className='session-info-container'>
+      <p>Pinterest helps you find ideas to try.</p>
+      <button type='button' className='info-btn'>How it works</button>
     </div>
-  );
-};
+  </div>
+);
+
 
 export default SessionPage;
