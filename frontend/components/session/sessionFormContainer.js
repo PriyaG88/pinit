2f8aba948ade34ff64000eab0853fef45663f9a9
PriyaG88 <priya.g71488@gmail.com>
2018-04-21 23:55:11 -0700

    Remove login import

--- frontend/components/session/sessionFormContainer.js
+++ frontend/components/session/sessionFormContainer.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux';
-import { signup, login } from '../../actions/sessionActions';
+import { signup } from '../../actions/sessionActions';
 import SessionForm from './sessionForm';
 
 const mapDispatchToProps = dispatch => ({
-  signup: user => dispatch(signup(user)),
-  login: user => dispatch(login(user))
+  signup: user => dispatch(signup(user))
 });
 
 export default connect(null, mapDispatchToProps)(SessionForm);
