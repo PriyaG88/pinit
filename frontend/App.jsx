a179d7bc1ea0b7fc231c207835006084ae4a58a7
PriyaG88 <priya.g71488@gmail.com>
2018-04-29 23:51:55 -0700

    Add route for home component

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SessionPage from './components/session/sessionPage';
 import LoginPageContainer from './components/session/loginPageContainer';
+import Home from './components/home';
 
 const App = () => (
   <Switch>
     <Route path='/session' component={SessionPage} />
     <Route path='/login' component={LoginPageContainer} />
+    <Route path='/home' component={Home} />
   </Switch>
 );
 
