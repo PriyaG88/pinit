143ba5bf504a47d548f47363550ced335b0b051d
PriyaG88 <priya.g71488@gmail.com>
2018-05-03 17:47:06 -0700

    Add home container and replace home component with home container component in rendered routes

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SessionPage from './components/session/sessionPage';
 import LoginPageContainer from './components/session/loginPageContainer';
-import Home from './components/ui/home';
+import HomeContainer from './components/ui/homeContainer';
 
 const App = () => (
   <Switch>
     <Route path='/session' component={SessionPage} />
     <Route path='/login' component={LoginPageContainer} />
-    <Route path='/home' component={Home} />
+    <Route path='/home' component={HomeContainer} />
   </Switch>
 );
 
