6a1341afcb3462a6672608d8b0a6a2fbc30aacb8
PriyaG88 <priya.g71488@gmail.com>
2018-05-03 18:23:17 -0700

    Render profileContainer component

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import SessionPage from './components/session/sessionPage';
 import LoginPageContainer from './components/session/loginPageContainer';
 import HomeContainer from './components/ui/homeContainer';
+import ProfileContainer from './components/ui/profileContainer';
 
 const App = () => (
   <Switch>
     <Route path='/session' component={SessionPage} />
     <Route path='/login' component={LoginPageContainer} />
-    <Route path='/home' component={HomeContainer} />
+    <Route exact path='/' component={HomeContainer} />
+    <Route path='/:userName' component={ProfileContainer} />
   </Switch>
 );
 
