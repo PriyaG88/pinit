9fa3bc45b4a310839ba2b2061d040f3ecbdf983a
PriyaG88 <priya.g71488@gmail.com>
2018-05-04 17:10:58 -0700

    Remove age and replace with name

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417051228) do
+ActiveRecord::Schema.define(version: 20180504235526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.integer "age", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email"
     t.index ["session_token"], name: "index_users_on_session_token"
   end
