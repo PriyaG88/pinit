a06563ef9b2f60c410e4a71b85cf9435c8295f7c
PriyaG88 <priya.g71488@gmail.com>
2018-05-07 23:44:40 -0700

    Add conditional to only render component if path doesn't match explore path and refactor board into a separate component

--- frontend/components/ui/profile.jsx
+++ frontend/components/ui/profile.jsx
@@ -2,29 +2,38 @@ import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import Avatar from 'react-avatar';
 import NavBar from './navBar';
+import NewBoard from '../board/newBoard';
+const Profile = props => {
+  const { currentUser, location } = props;
 
-const Profile = ({ currentUser }) => (
-  <Fragment>
-    <NavBar currentUser={currentUser}/>
-    <div className='user-info-container'>
-      <div className='user-info'>
-        <div className='name'>
-          {currentUser.name}
-        </div>
-        <div className='follow-info'>
-          <div className='followers follow-info-item'>
-            <NavLink to={`/${currentUser.email}/followers`}>10 followers</NavLink>
+  if (location.pathname !== '/explore') {
+    return (
+      <Fragment>
+        <NavBar currentUser={currentUser}/>
+        <div className='user-info-container'>
+          <div className='user-info'>
+            <div className='name'>
+              {currentUser.name}
+            </div>
+            <div className='follow-info'>
+              <div className='followers follow-info-item'>
+                <NavLink to={`/${currentUser.email}/followers`}>10 followers</NavLink>
+              </div>
+              <div className='following follow-info-item'>
+                <NavLink to={`/${currentUser.email}/following`}>15 following</NavLink>
+              </div>
+            </div>
           </div>
-          <div className='following follow-info-item'>
-            <NavLink to={`/${currentUser.email}/following`}>15 following</NavLink>
+          <div className='user-avatar-lg'>
+            <Avatar name={currentUser.name} size={100} round={true} />
           </div>
         </div>
-      </div>
-      <div className='user-avatar-lg'>
-        <Avatar name={currentUser.name} size={100} round={true} />
-      </div>
-    </div>
-  </Fragment>
-);
+        <NewBoard />
+      </Fragment>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default Profile;
