71da6ce38cc454e573d3ca108d665fcca4225e9f
PriyaG88 <priya.g71488@gmail.com>
2018-05-09 21:24:36 -0700

    Update session and login routes to use Auth route to redirect if user is already logged in

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AuthRoute from './util/route_util';
 import SessionPage from './components/session/sessionPage';
 import LoginPageContainer from './components/session/loginPageContainer';
 import HomeContainer from './components/ui/homeContainer';
@@ -7,8 +8,8 @@ import ProfileContainer from './components/ui/profileContainer';
 
 const App = () => (
   <Switch>
-    <Route path='/session' component={SessionPage} />
-    <Route path='/login' component={LoginPageContainer} />
+    <AuthRoute path='/session' component={SessionPage} />
+    <AuthRoute path='/login' component={LoginPageContainer} />
     <Route exact path='/' component={HomeContainer} />
     <Route path='/:userName' component={ProfileContainer} />
   </Switch>
